91e1267587f5a0a9401de5677b7d6bab0695a953
Ewald Zietsman <ewald@siyavula.com>
2014-04-04 15:50:09 +0200

    small refactor into OO design

--- epub2html/epub2html.py
+++ epub2html/epub2html.py
@@ -1,35 +1,63 @@
 import sys
 import os
 import zipfile
+import shutil
 
 from lxml import etree
-
 try:
     import docopt
 except ImportError:
     print("Could not find docopt, please install using: sudo easy-install docopt")
     sys.exit(1)
 
-
-__tempfolder = "_temp"
-
-def unzip_epub_into_temp(source_filename, dest_dir=__tempfolder):
-    """ Extract the epub contents into a temporary folder
-    """
-    with zipfile.ZipFile(source_filename) as zf:
-        zf.extractall(dest_dir)
+import settings
 
 
-def clean_temp_folder():
-    import shutil
+class EpubReader:
+    def __init__(self):
+        ''' Class to parse and convert epub into set of static web pages.
+        '''
+        self.settings = settings.settings
+        self._tempfolder = settings.settings["tempfolder"] 
+        self._sitefolder = settings.settings["sitefolder"] 
 
-    shutil.rmtree(__tempfolder)
+        return None
+
+
+    def readEpub(self, epub):
+        ''' Open epub and write it to a folder where it will be served statically.
+        Output folder is specified in settings.py
+        '''
+
+        self.unzip_epub_into_temp(epub)
+
+        return None
+
+
+    def unzip_epub_into_temp(self, source_filename, dest_dir=None):
+        ''' Extract the epub contents into a temporary folder
+        '''
+        if dest_dir is None:
+            dest_dir = self._tempfolder
+
+        with zipfile.ZipFile(source_filename) as zf:
+            zf.extractall(dest_dir)
+
+        return None
+
+
+    def clean_temp_folder(self):
+        ''' Clean up the temp folder
+        '''
+        shutil.rmtree(self._tempfolder)
+
+        return None
+
 
 
 if __name__ == "__main__":
     args = sys.argv
 
-    print(args)
-
-    unzip_epub_into_temp(args[1])
-    clean_temp_folder()
+    epubreader = EpubReader()
+    epubreader.readEpub(sys.argv[1])
+    epubreader.clean_temp_folder()
